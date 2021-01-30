97c4d140ce82ebd0e8ba8eb4e43775e81bdb23c0
zoolake <junhomoon224@gmail.com>
2021-01-30 23:22:10 +0900

    blog-backend-temp

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,26 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-    console.log(ctx.url);
-    console.log(1);
-    if(ctx.query.authorized !== '1') {
-        ctx.status = 401; // Unauthorized
-        return;
-    }
-    await next();
-    console.log('END');
-});
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
 
-app.use((ctx, next) => {
-    console.log(2);
-    next();
-});
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
-app.use(ctx => {
-    ctx.body = 'hello world';
-});
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());  
 
 app.listen(4000, () => {
     console.log('Listening to port 4000');
