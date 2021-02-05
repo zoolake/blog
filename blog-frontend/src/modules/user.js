b60d84571330d94157ee96784e444068436d8907
zoolake <junhomoon224@gmail.com>
2021-02-05 23:42:33 +0900

    Frontend-Project_v1

---
+++ blog-frontend/src/modules/user.js
@@ -0,0 +1,10 @@
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
+import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga';
+
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
+// 회원 정보 확인
+const [CHECK ,CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
+    'user/CHECK'
+);
